839ad9e7fc140e445db86f1a544e8f47ebbb90e7
Benjámin Józsa <benjamin.jozsa@sensomediagroup.com>
2022-10-23 15:18:42 -0700

    feat(be): add user model

--- cc-backend/src/app.module.ts
+++ cc-backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost:27017/calorieCounter',
+    ),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
