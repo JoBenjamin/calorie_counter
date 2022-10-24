13c21c7b33138657c67eecc950a6f99c1c81e6aa
Benjámin Józsa <benjamin.jozsa@sensomediagroup.com>
2022-10-24 01:14:08 -0700

    feat(fe): add basic validation to new food entry

--- cc-frontend/src/components/AddEntryWidget.vue
+++ cc-frontend/src/components/AddEntryWidget.vue
@@ -1,4 +1,41 @@
 <template lang="pug">
 .w-full.pb-5.pt-3
-    PButton(label="New entry" icon="pi pi-plus")
+    PButton(label="New entry" icon="pi pi-plus" @click="dialogOpen = true")
+    Dialog(v-model:visible="dialogOpen" header="Add new food entry").w-4
+        .card
+            .field.h-5rem
+                label(for="name") Name
+                InputText(id="name" v-model="$v.name.$model" :class="{'p-invalid': $v.name.$error}").w-full
+                small.p-error.h-1rem {{$v.name.$error ? $v.name.$errors[0].$message : ' '}}
+            .field.h-5rem
+                label(for="calorie") Calories
+                InputNumber(id="calorie" v-model="$v.calories.$model" :class="{'p-invalid': $v.calories.$error}").w-full
+                small.p-error {{$v.calories.$error ? $v.calories.$errors[0].$message : ' '}}
+            .field.h-5rem
+                label(for="date") Date
+                Calendar(id="date" v-model="formState.date" :max-date="new Date()").w-full
+        template(#footer)
+            PButton(label="Submit" @click="$v.$validate")
+            PButton(label="Cancel" class="p-button-secondary" @click="dialogOpen = false")
 </template>
+
+<script lang="ts" setup>
+import { reactive, ref } from "vue";
+import { useVuelidate } from "@vuelidate/core";
+import { required, minValue } from "@vuelidate/validators";
+
+const formState = reactive({
+  name: "",
+  calories: 0,
+  date: new Date(),
+});
+
+const validationRules = {
+  name: { required },
+  calories: { required, minValue: minValue(1) },
+};
+
+const $v = useVuelidate(validationRules, formState);
+
+const dialogOpen = ref(false);
+</script>
--- cc-frontend/src/main.ts
+++ cc-frontend/src/main.ts
@@ -1,3 +1,5 @@
+/* eslint-disable vue/no-reserved-component-names */
+/* eslint-disable vue/multi-word-component-names */
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
@@ -6,12 +8,18 @@ import PrimeVue from "primevue/config";
 import Button from "primevue/button";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
+import Dialog from "primevue/dialog";
+import InputText from "primevue/inputtext";
+import InputNumber from "primevue/inputnumber";
+import Calendar from "primevue/calendar";
+
 import axios from "axios";
 
 const app = createApp(App);
 const pinia = createPinia();
 
 axios.interceptors.request.use((config) => {
+  if (!config || !config.headers) return config;
   config.headers.authorization = `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`;
   return config;
 });
@@ -28,6 +36,10 @@ app.use(pinia);
 app.component("PButton", Button);
 app.component("DataTable", DataTable);
 app.component("Column", Column);
+app.component("Dialog", Dialog);
+app.component("Calendar", Calendar);
+app.component("InputText", InputText);
+app.component("InputNumber", InputNumber);
 
 app.use(router);
 
