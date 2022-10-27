f7367bc7d9702c47ffb51cf269134c51296223b5
Benjámin Józsa <benjamin.jozsa@sensomediagroup.com>
2022-10-27 15:05:32 -0700

    feat(fe): adjust error handling on auth request

--- cc-frontend/src/stores/auth.ts
+++ cc-frontend/src/stores/auth.ts
@@ -7,13 +7,18 @@ export const useAuthStore = defineStore("auth", {
   state: () => ({ email: "", authError: false }),
   actions: {
     async fetchAuth() {
-      const response = await axios.get(`${baseUrl}/auth/whoami`);
-      if (!response.data || !response.data.email) {
+      try {
+        const response = await axios.get(`${baseUrl}/auth/whoami`);
+        if (!response.data || !response.data.email) {
+          this.authError = true;
+          return;
+        }
+        this.email = response.data.email;
+        this.authError = false;
+      } catch (error) {
+        console.log(error);
         this.authError = true;
-        return;
       }
-      this.email = response.data.email;
-      this.authError = false;
     },
   },
 });
