2cc9660153ea17af720ede4e14c08314656f6111
Benjámin Józsa <benjamin.jozsa@sensomediagroup.com>
2022-10-27 16:57:31 -0700

    feat(be): add role guard to delete food record endpoint

--- cc-backend/src/foodrecord/foodrecord.controller.ts
+++ cc-backend/src/foodrecord/foodrecord.controller.ts
@@ -1,9 +1,23 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { createFoodRecordDto } from './foodrecord.dto';
 import { FoodRecord, FoodRecordDocument } from './foodrecord.schema';
 import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserRolesEnum } from 'src/user/user.schema';
+import { RolesGuard } from 'src/guards/roles.guard';
 
 @Controller('foodrecord')
 @UseGuards(AuthGuard('bearer'))
@@ -36,5 +50,16 @@ export class FoodrecordController {
       foodRecords = await this.foodRecordModel.find({ user: req.user._id });
     }
     return foodRecords;
+  }
+
+  @Delete('/:id')
+  @Roles(UserRolesEnum.ADMIN)
+  @UseGuards(RolesGuard)
+  async deleteFoodRecord(@Param('id') id) {
+    if (!mongoose.isValidObjectId(id)) {
+      throw new HttpException('INVALID_ID', HttpStatus.BAD_REQUEST);
+    }
+    const result = await this.foodRecordModel.findByIdAndDelete(id);
+    return result;
   }
 }
